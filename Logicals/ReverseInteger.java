2da37e3fc55593ab2372357456e033bd505de26a
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-01-31 19:24:29 +0530

    Day 2

---
+++ Logicals/ReverseInteger.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int x = Sc.nextInt();
+        int ans = reverse(x);
+        System.out.println(ans);
+
+    }
+
+    public static int reverse(int n) {
+        if (n == 0) {
+            return n;
+        }
+        long reversed = 0;
+        while (n > 0) {
+            int dig = n % 10;
+            reversed = reversed * 10 + dig;
+            n = n / 10;
+        }
+        if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) reversed;
+
+    }
+}
