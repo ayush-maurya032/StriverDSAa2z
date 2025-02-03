017b682dd0edef1a755d8fa666687f3e241a57dd
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-02-03 14:19:07 +0530

    3rd feb

---
+++ Arrays/CheckSorted.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CheckSorted {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        System.out.println("enter 10 numbers");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Sc.nextInt();
+        }
+        System.out.println(check(arr));
+    }
+
+        public static boolean check(int[] arr){
+            for(int i =1; i<arr.length;i++){
+                if(arr[i]<arr[i-1]){
+                  return false;
+                }
+            }
+            return true;
+        }
+    
+    
+}
