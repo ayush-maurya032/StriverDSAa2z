527101c243f14421e754e05ef98f623b05007ba2
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-02-09 23:56:46 +0530

    done

---
+++ Arrays/MaximumConsecutiveOnes.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class MaximumConsecutiveOnes {
+
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter the limit");
+        int n = Sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("enter numbers");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Sc.nextInt();
+        }
+
+        int ans  = findMaxConsecutiveOnes(arr);
+        System.out.println(ans);
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int n = nums.length;
+        int maxCount = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                maxCount = Math.max(maxCount, count);
+                count = 0;
+            } else {
+                count++;
+            }
+
+        }
+        return Math.max(maxCount, count);
+    }
+}
