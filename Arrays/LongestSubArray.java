2ed659fa74ae3b5b26049d9e1befb51768e1a93e
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-02-17 16:09:44 +0530

    Subarray

--- Arrays/LongestSubArray.java
+++ Arrays/LongestSubArray.java
@@ -1,21 +1,21 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class LongestSubArray {
     public static void main(String[] args) {
- Scanner Sc = new Scanner(System.in);
-        System.out.println("Enter the limit");
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter the sum value");
         int k = Sc.nextInt();
 
-        int[] arr = new int[6];
-        System.out.println("enter numbers");
-        for (int i = 0; i < 6; i++) {
-            arr[i] = Sc.nextInt();
-        }
+        // nt[] arr = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        int[] arr = { 2, -1, 3, -2, 4, -3, 1, 5 };
 
-        System.out.println(longestsubarraybrutest(arr, k));
-        System.out.println(longestsubarraybetter1(arr, k));
+        // ystem.out.println(longestsubarraybrutest(arr, k));
+        // stem.out.println(longestsubarraybetter1(arr, k));
+        System.out.println(longestsubarrayslidingwindow(arr, k));
     }
-    //O(n^3)
+
+    // O(n^3)
     public static int longestsubarraybrutest(int[] arr, int k) {
 
         int n = arr.length;
@@ -24,7 +24,7 @@ public class LongestSubArray {
         for (int i = 0; i < n; i++) {
             // loop for ending index
             for (int j = i; j < n; j++) {
-                 //sum
+                // sum
                 long s = 0;
                 for (int m = i; m <= j; m++) {
                     s = s + arr[m];
@@ -39,15 +39,7 @@ public class LongestSubArray {
         return len;
     }
 
-
-
-
-
-
-
-
-
-    public static int longestsubarraybetter1(int[] arr, int k ){
+    public static int longestsubarraybetter1(int[] arr, int k) {
 
         int n = arr.length;
         int len = 0;
@@ -55,10 +47,9 @@ public class LongestSubArray {
         for (int i = 0; i < n; i++) {
             long s = 0;
             for (int j = i; j < n; j++) {
-                 //sum
+                // sum
                 s += arr[j];
-               
-                
+
                 if (s == k) {
                     len = Math.max(len, j - i + 1);
                 }
@@ -67,6 +58,86 @@ public class LongestSubArray {
 
         }
         return len;
+
+    }
+
+    // kadane
+    public static int longestsubarrayoptimal(int[] arr, int k) {
+        int n = arr.length;
+        int sum = 0;
+        int start = 0;
+        int ansEnd = 0;
+        int ansStart = 0;
+        for (int i = 0; i < n; i++) {
+            if (sum == 0) {
+                start = i;
+            }
+            sum += arr[i];
+
+            if (sum >= k) {
+                ansStart = start;
+                ansEnd = i;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return (ansEnd - ansStart + 1);
+    }
+
+    public static int longestsubarrayusinghashmap(int[] arr, int k) {
+
+        int n = arr.length;
+        HashMap<Integer, Integer> preSumMap = new HashMap<>();
+        int sum = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            if (sum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+            int rem = sum - k;
+
+            if (preSumMap.containsKey(rem)) {
+                int len = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+            if (!preSumMap.containsKey(sum)) {
+                preSumMap.put(sum, i);
+            }
+
+        }
+        return maxLen;
+    }
+
+    // two pointer approach for negatives too
+    public static int longestsubarrayslidingwindow(int[] arr, int k) {
+
+        int n = arr.length;
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        long sum = arr[0];
+        while (right < n) {
+           
+
+            while (left <= right && sum > k) {
+                sum -= arr[left];
+                left++;
+            }
+
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+
+            right++;
+            if (right < n) {
+                sum += arr[right];
+            }
+        }
+        return maxLen;
 
     }
 
