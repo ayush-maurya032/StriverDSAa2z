0dfcab97b54501ed3e18b4d5a7a351cacc3f1458
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-02-22 23:43:08 +0530

    Two Sum done

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -3,23 +3,33 @@ import java.util.HashMap;
 
 public class TwoSum {
     public static void main(String[] args) {
-        
-        int[] arr = {2,5,7,8,0};
+
+        int[] arr = { 2, 5, 7, 8, 0 };
         int target = 9;
-        System.out.println(Arrays.toString(twoSum(arr,target)));
-
-
+        System.out.println(Arrays.toString(twoSumOptimal(arr, target)));
 
     }
 
-    
-    public static  int[] twoSum(int[] nums, int target) {
+    public static int[] twoSumbrute(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] { -1, -1 };
+
+    }
+
+    public static int[] twoSumbetter(int[] nums, int target) {
         // for(int i = 0; i < nums.length; i++){
-        //     for(int j = i+1; j < nums.length; j++){
-        //         if(nums[i] + nums[j] == target){
-        //             return new int[] {i,j};
-        //         }
-        //     }
+        // for(int j = i+1; j < nums.length; j++){
+        // if(nums[i] + nums[j] == target){
+        // return new int[] {i,j};
+        // }
+        // }
         // }
         // return new int[]{-1,-1};
         int[] ans = new int[2];
@@ -39,5 +49,29 @@ public class TwoSum {
         return ans;
 
     }
-}
+ 
+      /// but this will ruin the answer since sorting is done...(only applicable on sorted arrays)
+    public static int[] twoSumOptimal(int[] nums, int target) {
+        Arrays.sort(nums);
+     
+        int left = 0;
+        int right = nums.length - 1;
 
+        int[] ans = new int[2];
+        ans[0] = ans[1] = -1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+
+            if (sum == target) {
+                return new int[]{left, right}; // Return immediately after finding the answer
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1}; // No solution found
+    }
+    }
