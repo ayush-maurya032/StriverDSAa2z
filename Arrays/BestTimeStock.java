2f4ab9feabd818263757abd58c14aa646482db19
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-02-27 04:36:40 +0530

    done

---
+++ Arrays/BestTimeStock.java
@@ -0,0 +1,17 @@
+public class BestTimeStock {
+    public static void main(String[] args) {
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            min = Math.min(min, prices[i]);
+            maxprofit = Math.max(maxprofit, prices[i] - min);
+
+        }
+        return maxprofit;
+    }
+}
