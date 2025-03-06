e39c0811848f3e9b32d497b4b1afd695c56e30d6
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-03-06 23:21:23 +0530

    Intersection in array

---
+++ Arrays/Intersection.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Intersection {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        // System.out.println("Enter the limit of arr1");
+        // int n1 = Sc.nextInt();
+        // int[] arr1 = new int[n1];
+        // System.out.println("enter the limit of arr2");
+        // int n2 = Sc.nextInt();
+        // int[] arr2 = new int[n2];
+
+        // System.out.println("Enter" + n1 + "elements in array 1");
+        // for (int i = 0; i < n1; i++) {
+        // System.out.println("enter the element at index : " + i);
+        // arr1[i] = Sc.nextInt();
+
+        // }
+        // System.out.println("Enter" + n1 + "elements in array 1");
+        // for (int i = 0; i < n2; i++) {
+        // System.out.println("enter the element at index : " + i);
+        // arr2[i] = Sc.nextInt();
+
+        // }
+        int arr1[] = { 1, 2, 2, 3, 3, 4, 5, 6 };
+        int arr2[] = { 2, 3, 3, 5, 6, 6, 7 };
+
+        System.out.println(Arrays.toString(brute(arr1, arr2)));
+        System.out.println(Arrays.toString(optimal(arr1, arr2)));
+        Sc.close();
+    }
+
+    // Brute force solution
+    // the arrays are in sorted manner
+    public static int[] brute(int[] arr1, int[] arr2) {
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        int[] visited = new int[Math.min(n1, n2)];
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                if (arr1[i] == arr2[j] && visited[j] == 0) {
+                    ans.add(arr1[i]);
+                    visited[j] = 1;
+                    break;
+
+                }
+                if (arr2[j] > arr1[i]) {
+                    break;
+                }
+            }
+        }
+        int n = ans.size();
+        int[] newans = new int[n];
+        for (int num = 0; num < n; num++) {
+            newans[num] = ans.remove(0);
+        }
+        return newans;
+    }
+
+   public static int[] optimal(int[] arr1, int[] arr2){
+
+    int n1 = arr1.length;
+    int n2 = arr2.length;
+    int i = 0; int j =0;
+    ArrayList<Integer> ans = new ArrayList<>();
+    while(i<n1){
+
+        if(arr1[i] == arr2[j]){
+            ans.add(arr1[i]);
+            i++;
+            j++;
+        }
+        else{
+            i++;
+        }
+    }
+
+    int n = ans.size();
+    int[] newans = new int[n];
+        for (int num = 0; num < n; num++) {
+            newans[num] = ans.remove(0);
+        }
+        return newans;
+   }
+
+}
--- Arrays/RotateArray.java
+++ Arrays/RotateArray.java
@@ -7,74 +7,70 @@ import java.util.Scanner;
 //Left Rotation (Your Way): Reverse → Reverse first n-k → Reverse last k
 public class RotateArray {
     public static void main(String[] args) {
-         Scanner Sc = new Scanner(System.in);
+        Scanner Sc = new Scanner(System.in);
         System.out.println("Enter the limit");
         int n = Sc.nextInt();
         int[] arr = new int[n];
         System.out.println("enter numbers ");
         System.out.println("Enter the frequency of rotation");
         int k = Sc.nextInt();
-        
-        for (int i = 0; i <= n-1; i++) {
-            System.out.println("enter the element at index : "+ i);
+
+        for (int i = 0; i <= n - 1; i++) {
+            System.out.println("enter the element at index : " + i);
             arr[i] = Sc.nextInt();
-        
+
         }
-        rotatebetter(arr,k);
+        rotate(arr, k);
         System.out.println(Arrays.toString(arr));
         Sc.close();
 
     }
 
-
-    public static void  rotate(int[] nums, int k) {
+    public static void rotate(int[] nums, int k) {
         int n = nums.length;
-         k = k % n; // In case k > n
- 
-         // Step 1: Reverse the whole array
-         reverse(nums, 0, n - 1);
- 
-         // Step 2: Reverse first k elements
-         reverse(nums, 0, k - 1);
- 
-         // Step 3: Reverse the rest
-         reverse(nums, k, n - 1);
-     }
- 
- 
-     private static void reverse(int[] nums, int start, int end) {
-         while (start < end) {
-             int temp = nums[start];
-             nums[start] = nums[end];
-             nums[end] = temp;
-             start++;
-             end--;
-         }
-     }
+        k = k % n; // In case k > n
 
+        // Step 1: Reverse the whole array
+        reverse(nums, 0, n - 1);
 
-    //left rotation;
-     public static void rotatebetter(int[] nums, int k){
+        // Step 2: Reverse first k elements
+        reverse(nums, 0, k - 1);
+
+        // Step 3: Reverse the rest
+        reverse(nums, k, n - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    // left rotation;
+    public static void rotatebetter(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
         ArrayList<Integer> list = new ArrayList<>();
-        //temporary dtoring the k elements from beginning
-        for(int i =0; i < k; i++){
+        // temporary dtoring the k elements from beginning
+        for (int i = 0; i < k; i++) {
             list.add(nums[i]);
         }
 
-        //shifting
-        for(int i =k; i<n; i++){
-            nums[i-k] = nums[i];
+        // shifting
+        for (int i = k; i < n; i++) {
+            nums[i - k] = nums[i];
         }
-        
-       //Placing the shifters
 
-        for(int i = n-k; i<n; i++){
+        // Placing the shifters
+
+        for (int i = n - k; i < n; i++) {
             nums[i] = list.remove(0);
         }
 
-     }
- 
+    }
 
 }
