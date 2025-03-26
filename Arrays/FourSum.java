452516fc25435ff4713acb0b9153d485cde3cfae
ayush-maurya032 <032001ayushmaurya@gmail.com>
2025-03-27 00:45:50 +0530

    Leetcode 18. 4Sum

--- Arrays/FourSum.java
+++ Arrays/FourSum.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -8,6 +9,7 @@ public class FourSum {
     public static void main(String[] args) {
         int[] arr = { -1, 0, -1, 0, -2, 2 };
         System.out.println(fourSumBetter(arr, 0));
+        System.out.println(fourSumOptimal(arr, 0));
     }
 
     public static List<List<Integer>> fourSumBetter(int[] nums, int target) {
@@ -41,7 +43,54 @@ public class FourSum {
 
     }
 
-    public static void foursumOptimal(int[] arr) {
+    public static List<List<Integer>> fourSumOptimal(int[] nums, int target) {
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        // sorting the array
+        Arrays.sort(nums);
 
+        // ..Calculating the quadruplets
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                // 2 POINTER
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+
+                    if (sum == target) {
+                        ArrayList<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1]) {
+                            k++;
+                        }
+                        while (k < l && nums[l] == nums[l + 1]) {
+                            l--;
+                        }
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 }
