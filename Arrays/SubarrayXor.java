3cc16259f00b3258258ac4b604d4ba24f1dadb6e
ayush-maurya032 <032001ayushmaurya@gmail.com>
2025-03-27 22:37:59 +0530

    Gfg Count Subarray with given XOR

--- Arrays/SubarrayXor.java
+++ Arrays/SubarrayXor.java
@@ -1,10 +1,14 @@
 // Gfg : Count Subarrays with given XOR
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarrayXor {
     public static void main(String[] args) {
         int[] arr = { 4, 2, 2, 6, 4 };
         System.out.println(subarrayXorbetter(arr, 6));
         System.out.println(subarrayXorbrute(arr, 6));
+        System.out.println(subarrayXorOptimal(arr, 6));
     }
 
     public static int subarrayXorbrute(int arr[], int k) {
@@ -42,7 +46,31 @@ public class SubarrayXor {
         return cnt;
     }
 
+    public static int subarrayXorOptimal(int arr[], int k) {
+        // code here
+        int n = arr.length;
+        int xr = 0;
+        // prefix sum like usage here
 
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            xr = xr ^ arr[i];
+            int x = xr ^ k;
 
+            if (map.containsKey(x)) {
+                cnt += map.get(x);
+            }
+
+            // insert prefix in map till index
+            if (map.containsKey(xr)) {
+                map.put(xr, map.get(xr) + 1);
+            } else {
+                map.put(xr, 1);
+            }
+        }
+        return cnt;
+    }
 
 }
