c1cd4852d5d398a427d5f548a0b5661161b5277e
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-04-01 00:54:57 +0530

    Brute force Striver

--- Arrays/MergeSortedArrays.java
+++ Arrays/MergeSortedArrays.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
-
+//This code works on the striver question but leetcode question is different but can be modified for leetcode question too 
 public class MergeSortedArrays{
     public static void main(String[] args) {
-        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums1 = {1,2,3};
         int[] nums2 = {2,5,6};
         mergeBrute(nums1,3,nums2,3);
         System.out.println(Arrays.toString(nums1));
-
+        System.out.println(Arrays.toString(nums2));
+        
     }
     public static void mergeBrute(int[] nums1, int m, int[] nums2, int n) {
         int[] nums3 = new int[n + m];
@@ -40,12 +41,21 @@ public class MergeSortedArrays{
         }
       
         for( i = 0; i < m + n; i++){
-          nums1[i] = nums3[i];
+          if(i < m){
+            nums1[i] = nums3[i];
+          }
+          else{
+            nums2[i-n] = nums3[i];
+          }
         }
   
   
   
   
       }
+
+
+
+     
 
 }
