2dc221614cbd795ad522fbb6f04e7e815ad0946a
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-04-01 00:06:39 +0530

    Brute force using 3rd array

--- Arrays/MergeSortedArrays.java
+++ Arrays/MergeSortedArrays.java
@@ -4,32 +4,48 @@ public class MergeSortedArrays{
     public static void main(String[] args) {
         int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {2,5,6};
-        merge(nums1,3,nums2,3);
+        mergeBrute(nums1,3,nums2,3);
         System.out.println(Arrays.toString(nums1));
 
     }
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m - 1;  // Last valid element in nums1
-         int p2 = n - 1;  // Last element in nums2
-         int p = m + n - 1;  // Last index of nums1
- 
-         while (p1 >= 0 && p2 >= 0) {
-             if (nums1[p1] > nums2[p2]) {
-                 nums1[p] = nums1[p1];
-                 p1--;
-             } else {
-                 nums1[p] = nums2[p2];
-                 p2--;
-             }
-             p--;
-         }
- 
-         // If there are remaining elements in nums2, copy them
-         while (p2 >= 0) {
-             nums1[p] = nums2[p2];
-             p2--;
-             p--;
-         }
-     }
+    public static void mergeBrute(int[] nums1, int m, int[] nums2, int n) {
+        int[] nums3 = new int[n + m];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+  
+        while( i < m && j < n){
+          if(nums1[i] <= nums2[j]){
+              nums3[k] = nums1[i];
+              i++;
+              k++;
+          }
+          else{
+              nums3[k] = nums2[j];
+              j++;
+              k++;
+          }
+        }
+  
+        while(i < m){
+          nums3[k] = nums1[i];
+          i++;
+          k++;
+        }
+  
+        while(j < n){
+          nums3[k] = nums2[j];
+          j++;
+          k++;
+        }
+      
+        for( i = 0; i < m + n; i++){
+          nums1[i] = nums3[i];
+        }
+  
+  
+  
+  
+      }
 
 }
