b2b8fffdd382f601588db453a482632ca06584d7
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-04-04 14:45:55 +0530

    LeetCode 152. Maximum Product Subarray

--- Arrays/MaximumProductSubarray.java
+++ Arrays/MaximumProductSubarray.java
@@ -4,5 +4,25 @@ The Optimal approaches include a intution based logical approach and other using
 Kadane is less intuitive. So will take the logical and intuitive approach here.
 */
 public class MaximumProductSubarray{
-    psvm
+    public static void main(String[] args) {
+        int[] array = {2,3,-2,4};
+        System.out.println(maxProductSubArray(array));
+        
+    }
+
+    public static int maxProductSubArray(int[] arr) {
+        int n = arr.length; //size of array.
+
+        int pre = 1, suff = 1;
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (pre == 0) pre = 1;
+            if (suff == 0) suff = 1;
+            pre *= arr[i];
+            suff *= arr[n - i - 1];
+            ans = Math.max(ans, Math.max(pre, suff));
+        }
+        return ans;
+    }
+
 }
