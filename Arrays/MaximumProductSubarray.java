c5ad1f8301de684ea524f6677a5108ce25a56ad7
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-04-04 14:36:35 +0530

    Leetcode 152. Maximum Product Subarray

---
+++ Arrays/MaximumProductSubarray.java
@@ -0,0 +1,8 @@
+/* LeetCode 152. Maximum Product Subarray
+The brute  and better force approaach is same as maximum sum subarray with o(ncube) and o(nsquare)
+The Optimal approaches include a intution based logical approach and other using Kadane's .
+Kadane is less intuitive. So will take the logical and intuitive approach here.
+*/
+public class MaximumProductSubarray{
+    psvm
+}
