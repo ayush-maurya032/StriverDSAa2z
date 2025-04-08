415a5fc140237c5378dead88b944fb5bb8a58d7f
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-04-08 21:35:18 +0530

    Binary Search CeilandFloor using bs

--- Binary_Search/CeilTheFloor.java
+++ Binary_Search/CeilTheFloor.java
@@ -11,6 +11,7 @@ public class CeilTheFloor{
         int[] arr = {5, 6, 8, 9, 6, 5, 5, 6}; 
         int x = 7;
         System.out.println(Arrays.toString(getFloorAndCeil(x, arr)));
+        System.out.println(Arrays.toString(findFloorAndCeil(arr, x)));
         
     }
     public static int[] getFloorAndCeil(int x, int[] arr) {
@@ -35,5 +36,34 @@ public class CeilTheFloor{
         return new int[]{floor, ceil};
         
       
+    }
+
+
+
+    public static int[] findFloorAndCeil(int[] arr, int x) {
+        Arrays.sort(arr); // Step 1: Sort the array
+
+        int floor = -1;
+        int ceil = -1;
+        int low = 0, high = arr.length - 1;
+
+        // Step 2: Binary Search for Floor and Ceil
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == x) {
+                floor = arr[mid];
+                ceil = arr[mid];
+                break;
+            } else if (arr[mid] < x) {
+                floor = arr[mid];  // possible floor
+                low = mid + 1;
+            } else {
+                ceil = arr[mid];   // possible ceil
+                high = mid - 1;
+            }
+        }
+
+        return new int[]{floor, ceil};
     }
 }
