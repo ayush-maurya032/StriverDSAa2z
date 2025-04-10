34c27bcbf81978c3bb94efd501a537bebee2643a
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-04-11 00:19:51 +0530

    Implemented Lower and Upper Bound

--- Binary_Search/UpperBound.java
+++ Binary_Search/UpperBound.java
@@ -1,17 +1,17 @@
-public class LowerBound{
+public class UpperBound{
     public static void main(String[] args) {
         int[] arr = {1,2,3,3,5,8,8,10,10,11};
-        System.out.println(lowerBound(arr, 9));
+        System.out.println(upperBound(arr, 9));
     }
 
-    public static int lowerBound(int[] arr, int target){
+    public static int upperBound(int[] arr, int target){
         int n = arr.length;
         int low = 0;
         int high = n - 1;
         int ans = n;
         while(low <= high){
             int mid = low + (high - low) / 2;
-            if(arr[mid] >= target){
+            if(arr[mid] > target){
                 ans = mid;
                 high = mid - 1;
             }
