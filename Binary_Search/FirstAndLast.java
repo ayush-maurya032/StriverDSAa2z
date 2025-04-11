ecd7066a0a21d7f8aa80c8f6dff86aec348c2164
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-04-11 23:58:57 +0530

    First and Last occurence in array

---
+++ Binary_Search/FirstAndLast.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public static void main(String[] args){
+        int first = findFirstOccurrence(nums, target);
+        int last = findLastOccurrence(nums, target);
+        so
+    }
+
+    public  static int findFirstOccurrence(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1; // continue to search on the left
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int findLastOccurrence(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1; // continue to search on the right
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
