921d2254449efe9f56258e67718925f5d6ca8f35
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-04-18 23:46:49 +0530

    Minor change

--- Arrays/BinarySearch.java
+++ Arrays/BinarySearch.java
@@ -1,4 +1,4 @@
-public class BinarySearch{
+public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1,2,3,8,9, 13 ,38,71, 32};
         System.out.println(search(arr, 38));
