311eefd65b175c6d594aff9e4b3f3a9ab0e880fd
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-04-23 23:53:06 +0530

    Leetcode 1539. Kth Missing Positive Number

--- Binary_Search/KthMissingNumber.java
+++ Binary_Search/KthMissingNumber.java
@@ -2,6 +2,7 @@ public class KthMissingNumber{
     public static void main(String[] args) {
         int[] arr = {2, 3, 4, 7, 11};
         System.out.println(brute(arr, 5));
+        System.out.println(optimal(arr, 5));
     }
     public static int brute(int[] arr, int k){
         int n  = arr.length;
@@ -14,5 +15,23 @@ public class KthMissingNumber{
             }
         }
         return k;
+    }
+
+    public static int optimal(int[] arr, int k){
+        int n = arr.length;
+        int low = 0;
+        int high = n-1;
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+            int missing = arr[mid] - (mid + 1);
+            if(missing < k){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return low + k; // most important logic refer notes
     }
     }
