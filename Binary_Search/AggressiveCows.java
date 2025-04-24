f5d9542f146835c6152af3bc67a21ff71806c23d
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-04-25 00:01:37 +0530

    Aggressive cows new patterned qs of max of mins

---
+++ Binary_Search/AggressiveCows.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class AggressiveCows {
+    public static void main(String[] args) {
+        int[] stalls = { 3, 0, 4, 11, 9, 7 };
+        int k = 4;
+        System.out.println(aggressiveCows(stalls, k));
+
+    }
+
+    public static int aggressiveCows(int[] stalls, int k) {
+        // code here
+        Arrays.sort(stalls);
+        int n = stalls.length;
+        int low = 0;
+        int high = stalls[n - 1] - stalls[0];
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canWePlace(stalls, mid, k) == true) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return high;
+
+    }
+
+    static boolean canWePlace(int[] stalls, int dist, int cows) {
+        int cntCows = 1;
+        int last = stalls[0];
+        int n = stalls.length;
+        for (int i = 1; i < n; i++) {
+            if (stalls[i] - last >= dist) {
+                cntCows++;
+                last = stalls[i];
+            }
+            if (cntCows >= cows) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
