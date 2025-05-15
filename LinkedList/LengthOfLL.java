ca84ec4b50ba21f12180758e0c0070890f6a064b
Ayush Maurya <032001ayushmaurya@gmail.com>
2025-05-15 23:51:48 +0530

    Length of LL

---
+++ LinkedList/LengthOfLL.java
@@ -0,0 +1,29 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LengthOfLL {
+public static void main(String[] args) {
+    Node head = new Node(10);
+    head.next = new Node(11);
+    head.next.next = new Node(111);
+    head.next.next.next = new Node(2712);
+    System.out.println(length(head));
+    
+}
+
+    static int length(Node head) {
+        int cnt = 0;
+        while (head != null) {
+            cnt++;
+            head = head.next;
+        }
+        return cnt;
+    }
+}
